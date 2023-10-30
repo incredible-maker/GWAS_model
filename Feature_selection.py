df317fe5c179dfbd1792d8d9158cfd38ddf5772c
Jacky <117089393+incredible-maker@users.noreply.github.com>
2023-10-30 10:59:10 +0100

    Add files via upload

---
+++ Feature_selection.py
@@ -0,0 +1,69 @@
+import subprocess
+
+# 调用ANNOVAR
+def run_annovar(input_file, output_file, annovar_dir, build_ver="hg19"):
+    annotate_variation = annovar_dir + "/annotate_variation.pl"
+    command = [
+        annotate_variation,
+        "-out", output_file,
+        "-build", build_ver,
+        input_file,
+        annovar_dir + "/humandb/"
+    ]
+    subprocess.run(command)
+
+# 使用您的文件和ANNOVAR目录调用此函数
+run_annovar("your_input_file.txt", "your_output_file", "/path/to/annovar")
+
+
+import requests
+
+def get_pathways_for_genes(gene_list):
+    url = "https://reactome.org/ContentService/data/query/enhanced/low/diagram/entity/LLP/"
+    headers = {
+        "Accept": "application/json"
+    }
+    pathways = []
+    for gene in gene_list:
+        response = requests.get(url + gene, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            for item in data:
+                pathways.append(item['displayName'])
+    return set(pathways)
+
+genes = ["LRFN5", ...]  # 你的基因列表
+pathways = get_pathways_for_genes(genes)
+print(pathways)
+
+
+import torch.nn as nn
+
+class CombinedNN(nn.Module):
+    def __init__(self, num_genes, num_clinical_features):
+        super(CombinedNN, self).__init__()
+        
+        input_size = num_genes + num_clinical_features
+        hidden_size = ... # define as needed
+        
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, 1)  # Binary classification
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
+
+# For your data:
+# Assuming `genes_data` is your matrix of genetic data and `clinical_data` is the matrix of clinical features
+weighted_genes = genes_data * 0.5
+clinical_data[:, 0] *= 0.1  # Assuming 0th column is "weight gain"
+clinical_data[:, 1] *= 0.1  # Assuming 1st column is "loss of interest"
+clinical_data[:, 2] *= 0.15  # Assuming 2nd column is "insomnia"
+clinical_data[:, 3] *= 0.15  # Assuming 3rd column is "hypersomnia"
+
+combined_data = torch.cat((weighted_genes, clinical_data), dim=1)
---
+++ model_applied_neuron_network_with_lasso.py
@@ -0,0 +1,114 @@
+# Assuming X is your matrix of gene expressions (or SNPs) and y is the ICBT treatment score:
+model = Lasso(lam=1.0, lr=1.0, logistic=False)
+model.fit(X, y)
+
+# Get coefficients
+beta = model.getBeta()
+
+# Select genes with non-zero coefficients
+selected_genes = np.where(beta != 0)[0]
+X_selected = X[:, selected_genes]
+
+
+import torch
+from torch.utils.data import TensorDataset, random_split
+
+# Convert data to TensorDataset
+dataset = TensorDataset(torch.from_numpy(X_selected).float(), torch.from_numpy(y).float())
+
+# Compute lengths of splits
+train_length = int(0.2 * len(dataset))
+val_length = len(dataset) - train_length
+
+# Randomly split dataset
+train_dataset, val_dataset = random_split(dataset, [train_length, val_length])
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset, random_split
+
+# Convert data to TensorDataset
+dataset = TensorDataset(torch.from_numpy(X_selected).float(), torch.from_numpy(y).float().view(-1, 1))
+
+# Compute lengths of splits
+train_length = int(0.8 * len(dataset))
+val_length = len(dataset) - train_length
+
+# Randomly split dataset
+train_dataset, val_dataset = random_split(dataset, [train_length, val_length])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+
+# Define a simple feed-forward neural network
+class Net(nn.Module):
+    def __init__(self, input_size):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)
+
+    def forward(self, x):
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+model = Net(X_selected.shape[1])
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Train the model
+num_epochs = 100
+for epoch in range(num_epochs):
+    model.train()  # Set the model to training mode
+    for batch_X, batch_y in train_loader:
+        outputs = model(batch_X)
+        loss = criterion(outputs, batch_y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Validate the model
+    model.eval()  # Set the model to evaluation mode
+    val_loss = 0.0
+    with torch.no_grad():
+        for batch_X, batch_y in val_loader:
+            outputs = model(batch_X)
+            loss = criterion(outputs, batch_y)
+            val_loss += loss.item()
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Training Loss: {loss.item():.4f}, Validation Loss: {val_loss/len(val_loader):.4f}')
+
+
+def mean_absolute_error(y_pred, y_true):
+    return torch.mean(torch.abs(y_pred - y_true))
+
+def evaluate_model(model, data_loader, criterion_mse, criterion_mae):
+    model.eval()
+    total_mse = 0.0
+    total_mae = 0.0
+    total_samples = 0
+    
+    with torch.no_grad():
+        for batch_X, batch_y in data_loader:
+            outputs = model(batch_X)
+            total_mse += criterion_mse(outputs, batch_y).item() * batch_X.size(0)
+            total_mae += criterion_mae(outputs, batch_y).item() * batch_X.size(0)
+            total_samples += batch_X.size(0)
+    
+    avg_mse = total_mse / total_samples
+    avg_mae = total_mae / total_samples
+    
+    return avg_mse, avg_mae
+
+# After training the model, evaluate it
+criterion_mse = nn.MSELoss()
+criterion_mae = mean_absolute_error
+
+mse, mae = evaluate_model(model, val_loader, criterion_mse, criterion_mae)
+print(f'Mean Squared Error on Validation Set: {mse:.4f}')
+print(f'Mean Absolute Error on Validation Set: {mae:.4f}')
